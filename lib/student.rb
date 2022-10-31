af1e22319ea989ac56040f41168b721a43ac6e32
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-31 08:53:33 +0300

    complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn knowledge
+        @knowledge << knowledge
+        return @knowledge
+    end
 end
+#Footer
